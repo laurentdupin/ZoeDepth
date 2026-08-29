5f8b8e6e5199973ac0603bba66f2b8b504cdc7f5
laurentdupin <48155177+laurentdupin@users.noreply.github.com>
2026-08-29 17:31:52 +0200

    Reuse reduced precision activation workspace

--- native/src/operators.h
+++ native/src/operators.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "vulkan.h"
+#include <inferbridge/native_harness_int8_workspace.h>
 
 #include <cstdint>
 
@@ -229,6 +230,7 @@ public:
 
 private:
     VulkanContext& context_;
+    inferbridge::native::Int8ActivationWorkspace<VulkanBuffer> int8_workspace_;
     VulkanPipeline linear_;
     VulkanPipeline linear16_;
     VulkanPipeline linear_half_;
